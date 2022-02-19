cde85a8fffbddff5fb6603aa847343a003864213
Svetlana Pristaychuk <svetlana.pristajchuk@gmail.com>
2022-02-19 11:33:25 +0100

    fixed input

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -66,7 +66,7 @@ class Form extends Component {
         </div>
         <div>
           <input
-            type="tel"
+            type="name"
             name="number"
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
